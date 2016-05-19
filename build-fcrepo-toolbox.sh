e157856a3c165c88653ec656ccad81fa4c43032e
Benjamin Rosner <brosner@barnard.edu>
2016-05-19 14:32:17 -0400

    Uncommented a line.

--- build-fcrepo-toolbox.sh
+++ build-fcrepo-toolbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd fcrepo-camel-toolbox
-#MAVEN_OPTS="-Xmx1024m" mvn clean install
+MAVEN_OPTS="-Xmx1024m" mvn clean install
 cd ..
 ./servers/apache-karaf-4.0.5/bin/client -f configs/fcrepo-camel-toolbox.script
 rm servers/apache-karaf-4.0.5/etc/org.fcrepo.camel.indexing.triplestore.cfg
